52a0fc1192e0756040353ce4290cec7c5bd696fa
Yoon <yoondev93@gmail.com>
2021-04-25 13:50:12 +0900

    객체지향 - 데이터 타입

---
+++ 23. 객체지향 - 데이터 타입/데이터 타입.js
@@ -0,0 +1,10 @@
+/*
+- 기본형 타입(Primitive type)
+  > 숫자
+  > 문자열
+  > 불리언(true/false)
+  > null
+  > undefined
+
+- 참조형 타입(Reference type)
+*/
---
+++ 23. 객체지향 - 데이터 타입/레퍼 객체.js
@@ -0,0 +1,8 @@
+var str = 'coding';
+// var str = new String('coding');
+console.log(str.length); // 6
+console.log(str.charAt(0)); // "C"
+
+var str = 'coding';
+str.prop = 'everybody';
+console.log(str.prop); // undefined
