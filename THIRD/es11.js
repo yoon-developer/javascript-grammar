77d22f0f841d6a45e029fcf2bf4542b4e9e44355
Yoon <yoondev93@gmail.com>
2021-05-05 00:10:38 +0900

    es11

---
+++ THIRD/es11.js
@@ -0,0 +1,103 @@
+/**
+ * Optional Chaining (ES11)
+ * https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+ */
+ {
+    const person1 = {
+      name: 'Yoon',
+      job: {
+        title: 'S/W Engineer',
+        manager: {
+          name: 'Bob',
+        },
+      },
+    };
+    const person2 = {
+      name: 'Bob',
+    };
+  
+    // 💩💩💩💩💩💩
+    {
+      function printManager(person) {
+        console.log(person.job.manager.name);
+      }
+      printManager(person1);
+      // printManager(person2);
+    }
+  
+    // 💩💩💩
+    {
+      function printManager(person) {
+        console.log(
+          person.job
+            ? person.job.manager
+              ? person.job.manager.name
+              : undefined
+            : undefined
+        );
+      }
+      printManager(person1);
+      printManager(person2);
+    }
+  
+    // 💩
+    {
+      function printManager(person) {
+        console.log(person.job && person.job.manager && person.job.manager.name);
+      }
+      printManager(person1);
+      printManager(person2);
+    }
+  
+    // ✨
+    {
+      function printManager(person) {
+        console.log(person.job?.manager?.name);
+      }
+      printManager(person1);
+      printManager(person2);
+    }
+    console.clear();
+  }
+  
+  /**
+   * Nullish Coalescing Operator (ES11)
+   * https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
+   */
+  {
+    // Logical OR operator
+    // false: false, '', 0, null, undefined
+    {
+      const name = 'Yoon';
+      const userName = name || 'Guest';
+      console.log(userName);
+    }
+  
+    {
+      const name = null;
+      const userName = name || 'Guest';
+      console.log(userName);
+    }
+  
+    // 💩
+    {
+      const name = '';
+      const userName = name || 'Guest';
+      console.log(userName);
+  
+      const num = 0;
+      const message = num || 'undefined';
+      console.log(message);
+    }
+  
+    // ✨
+    {
+      const name = '';
+      const userName = name ?? 'Guest';
+      console.log(userName);
+  
+      const num = 0;
+      const message = num ?? 'undefined';
+      console.log(message);
+    }
+  }
